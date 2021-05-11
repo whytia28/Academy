23cbaa7de0b79a42503f657c6d1406da6667e6d6
whytia28 <wahyu.sp9@gmail.com>
2021-05-11 23:23:10 +0700

    layout selesai

--- app/src/main/java/com/example/academies/ui/detail/DetailCourseActivity.kt
+++ app/src/main/java/com/example/academies/ui/detail/DetailCourseActivity.kt
@@ -1,47 +1,25 @@
 package com.example.academies.ui.detail
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.academies.R
 import com.example.academies.databinding.ActivityDetailCourseBinding
+import com.example.academies.databinding.ContentDetailCourseBinding
 
 class DetailCourseActivity : AppCompatActivity() {
 
-    companion object {
-        const val EXTRA_COURSE = "extra_course"
-    }
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityDetailCourseBinding
+    private lateinit var detailCourseBinding: ContentDetailCourseBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityDetailCourseBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val activityDetailCourseBinding = ActivityDetailCourseBinding.inflate(layoutInflater)
+        detailCourseBinding = activityDetailCourseBinding.detailContent
 
-        setSupportActionBar(binding.toolbar)
+        setContentView(activityDetailCourseBinding.root)
 
+        setSupportActionBar(activityDetailCourseBinding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_detail_course)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_detail_course)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
