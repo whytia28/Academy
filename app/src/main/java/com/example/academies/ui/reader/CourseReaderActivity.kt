12d59c8b18762d3207eddd273741301d25bf8436
whytia28 <wahyu.sp9@gmail.com>
2021-05-11 19:52:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/academies/ui/reader/CourseReaderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.academies.ui.reader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.academies.R
+
+class CourseReaderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_reader)
+    }
+}
